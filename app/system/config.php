b2c3dee29c9cea407d81f4a4fcaf0c5faea3d036
Sm1l3 <rioly97.vg@gmail.com>
2018-04-21 23:44:24 +0200

    prepared statements fetchColumn

--- app/system/config.php
+++ app/system/config.php
@@ -25,20 +25,6 @@ $GLOBALS['redirect'] = "parent.frames['main_top'].location='main.php';";
 $quit = "parent.location='index.php';";
 
 
-
-function mysqli_result($res, $row = 0, $col = 0)
-{
-    $numrows = mysqli_num_rows($res);
-    if ($numrows && $row <= ($numrows - 1) && $row >= 0) {
-        mysqli_data_seek($res, $row);
-        $resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
-        if (isset($resrow[$col])) {
-            return $resrow[$col];
-        }
-    }
-    return false;
-}
-
 function goto_error($err_text)
 {
     echo '<br><b>Ошибка: </b><br>' . $err_text . '<br><INPUT TYPE="button" VALUE="Назад" onClick="history.back()">';
